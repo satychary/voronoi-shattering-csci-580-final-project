8177a04c68cb19f4e68bf5a304a1e29fc6c590d4
KevinZdoo@gmail.com <KevinZdoo@gmail.com@4f6fa130-d070-310d-0fd1-0289c6893db4>
2011-11-11 03:38:39 +0000

    add map and wrapper method, also change the Tetrahedron struct

--- trunk/580 Final/Project/Plug-In/VoronoiTmp/VoronoiTmp/VoronoiTmp/voronoiShatter.cpp
+++ trunk/580 Final/Project/Plug-In/VoronoiTmp/VoronoiTmp/VoronoiTmp/voronoiShatter.cpp
@@ -3,9 +3,88 @@
 
 VoronoiShatter::VoronoiShatter(void)
 {
+	currentKey = 0;
 }
 
 
 VoronoiShatter::~VoronoiShatter(void)
 {
+}
+
+//	Description: 
+//		get the tetrahedron form our pool by key
+//	Arguments:
+//		key   -- key for map retrieving
+//		tetra -- referenced, returned as the tetra associated with key  
+//	Return Value;
+//      true  -- if success
+//		false -- if key is not exist
+bool VoronoiShatter::getTetra(int key, Tetrahedron &tetra)
+{
+	TetraMapItr result = tetraPool.find(key);
+	if(result != tetraPool.end()){
+		tetra = result->second;
+		return true;
+	}
+	else
+		return false;
+}
+
+//	Description:
+//		add tetra to our pool
+//	Arguments:
+//		tetra -- tetra to be added
+//	Return Value;
+//		true  -- if success
+//		false -- if there is already a tetra with same key, 
+//               and our pool remain unchanged
+bool VoronoiShatter::addTetra(Tetrahedron tetra)
+{
+	TetraMapItem newItem(currentKey++, tetra);
+	return tetraPool.insert(newItem).second;
+}
+
+//	Description:
+//		delete a tetra from our pool by key
+//	Arguments:
+//		key -- key of tetra to delete
+//	Return Value;
+//		void
+void VoronoiShatter::deleteTetra(int key)
+{
+	tetraPool.erase(key);
+}
+
+//	Description:
+//		get the first tetra form our pool
+//	Arguments:
+//		tetra -- referenced, retruned as the first tetra
+//	Return Value;
+//		true  -- if success
+//		false -- if our pool is currently empty
+bool VoronoiShatter::firstTetra(Tetrahedron &tetra)
+{
+	TetraMapItr result= tetraPool.begin();
+	if(result != tetraPool.end()){
+		tetra = result->second;
+		return true;
+	}
+	else
+		return false;
+}
+
+//	Description:
+//		get AND delete a tetra from our pool by key
+//	Arguments:
+//		key   -- key to retrieve
+//		tetra -- referenced, returned as the tetra associated with key
+//	Return Value;
+bool VoronoiShatter::popTetra(int key, Tetrahedron &tetra)
+{
+	if(getTetra(key, tetra)){
+		deleteTetra(key);
+		return true;
+	}
+	else
+		return false;
 }
--- trunk/580 Final/Project/Plug-In/VoronoiTmp/VoronoiTmp/VoronoiTmp/voronoiShatter.h
+++ trunk/580 Final/Project/Plug-In/VoronoiTmp/VoronoiTmp/VoronoiTmp/voronoiShatter.h
@@ -8,23 +8,32 @@
 #define _voronoiShatter
 
 #include "maya/MPoint.h"
+#include <map>
+
+//  struct 
 struct Tetrahedron;
 
 struct Vertex{
 	MPoint point;
-    Tetrahedron *incidentTetra; // optional
+    int incidentTetra;        // optional, key of incident tetrahedron
 };
 
 struct Tetrahedron{
-	Vertex *v1;                // vertex
-	Vertex *v2;
-	Vertex *v3;
-	Vertex *v4;
-	Tetrahedron *neighbour1;   // neighbour
-	Tetrahedron *neighbour2;
-	Tetrahedron *neighbour3;
-	Tetrahedron *neighbour4;
+	Vertex v1;                // vertex
+	Vertex v2;
+	Vertex v3;
+	Vertex v4;
+	int neighbour1;           // key of neighbour
+	int neighbour2;
+	int neighbour3;
+	int neighbour4;
+	int key;                  // interger for map retrieving
 };
+
+// type define
+typedef std::map<int, Tetrahedron> TetraMap;                // tetrahedron map type
+typedef std::pair<int, Tetrahedron> TetraMapItem;           // tetrahedron item type
+typedef std::map<int, Tetrahedron>::iterator TetraMapItr;   // tetrahedron iterator type
 
 class VoronoiShatter
 {
@@ -51,7 +60,32 @@ public:
 	// Mian action
 	void perform();
 
+private:
 
+	// Private method
+	
+	// Wrapper method for map
+	bool getTetra(int key, Tetrahedron &tetra);         /* get a tetrahefron into tetra from our pool by key 
+														   return false if:
+														   key not exist
+														   */			
+	bool addTetra(Tetrahedron tetra);					/* add tetra to our pool,
+														   return false if:
+														   there is already a tetra with same key */
+	void deleteTetra(int key);							// delete tetra from our pool by key
+	bool firstTetra(Tetrahedron &tetra);				/* get the first tetrahedron into tetra from our pool
+														   return false if: our pool is empty*/
+
+
+	bool popTetra(int key,Tetrahedron &tetra);           /* get tetrahedron into tetra AND delete it form our pool by key,
+															return flase if:
+											                key not exist */
+
+	// Private member variable
+	TetraMap tetraPool;         /* our pool where we put all tetrahedrons avaliable now, 
+	                               each associated with an unique interger key value */
+
+	int currentKey;            // curent key value for new tehrahedron created
 };
 
 #endif
