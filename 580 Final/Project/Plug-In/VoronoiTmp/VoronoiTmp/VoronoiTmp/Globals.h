0b39afc6b4bab7cc378a38b881f4264bbb8fa856
KevinZdoo@gmail.com <KevinZdoo@gmail.com@4f6fa130-d070-310d-0fd1-0289c6893db4>
2011-11-20 07:54:08 +0000

    add file: Globals.h

---
+++ 580 Final/Project/Plug-In/VoronoiTmp/VoronoiTmp/VoronoiTmp/Globals.h
@@ -0,0 +1,14 @@
+//======================================================= 
+// File: Globals.h
+//
+// Authors: 
+//=========================================================
+
+#ifndef _vsGlobals
+#define _vsGlobals
+
+#define VS_SUCCESS 1                            // success
+#define VS_FAIL_RETRIEVE_POINT 2                // cant retrieve point from pool
+#define VS_FAIL_FIND_POINT 3                    // cant locate a tetra contain the point
+
+#endif
